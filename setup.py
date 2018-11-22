aef17396ce540b45a7a686380dae9dc7a4f36d44
MickaelRigault <mick.rigault@gmail.com>
2018-11-22 11:27:20 +0100

    end airmass and PA implemented

--- setup.py
+++ setup.py
@@ -10,8 +10,8 @@ MAINTAINER = 'Mickael Rigault'
 MAINTAINER_EMAIL = 'm.rigault@ipnl.in2p3.fr'
 URL = 'https://github.com/MickaelRigault/psfcube'
 LICENSE = 'Apache 2.0'
-DOWNLOAD_URL = 'https://github.com/MickaelRigault/psfcube/0.4'
-VERSION = '0.4.3'
+DOWNLOAD_URL = 'https://github.com/MickaelRigault/psfcube/0.5'
+VERSION = '0.5.0'
 
 try:
     from setuptools import setup, find_packages
